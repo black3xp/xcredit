62a851ac45fa49449184998077e9a1c8d9e0ef34
Vladimir Nunez <vnc3xp@gmail.com>
2022-06-29 16:05:44 -0400

    ...

--- api/models/Prestamos.js
+++ api/models/Prestamos.js
@@ -8,21 +8,49 @@
 module.exports = {
 
   attributes: {
-
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+    monto: {
+      type: 'number',
+      required: true
+    },
+    interes: {
+      type: 'number',
+      required: true
+    },
+    cuotas: {
+      type: 'number',
+      required: true
+    },
+    fecha: {
+      type: 'string',
+      required: true
+    },
+    cliente: {
+      model: 'Clientes'
+    },
+    usuario: {
+      model: 'Usuarios'
+    },
+    deleted: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    estado: {
+      type: 'string',
+      enum: ['Pendiente', 'Pagado', 'Cancelado'],
+      defaultsTo: 'Pendiente'
+    },
+    cuotasPagadas: {
+      type: 'number',
+      defaultsTo: 0
+    },
+    cuotasPendientes: {
+      type: 'number',
+      defaultsTo: 0
+    },
+    cuotasCanceladas: {
+      type: 'number',
+      defaultsTo: 0
+    }
   },
 
 };
